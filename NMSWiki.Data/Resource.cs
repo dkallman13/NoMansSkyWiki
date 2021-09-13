9bb4c1d7e3d04249ea06f2bb57dff5e0583463f0
Dan Kallman <dkallman13@gmail.com>
2021-09-13 16:51:07 -0400

    added the models for resources

--- NMSWiki.Data/Resource.cs
+++ NMSWiki.Data/Resource.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,18 @@ namespace NMSWiki.Data
 {
     public class Resource
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
+
+        [ForeignKey(nameof(PlanetResource))]
+        public List<int> PlanetResourceId { get; set; }
+        public virtual List<PlanetResource> PlanetResources { get; set; }
+
+        [ForeignKey(nameof(Ingredient))]
+        public List<int> IngredientId { get; set; }
+        public virtual List<Ingredient> Ingredients { get; set; }
     }
 }
