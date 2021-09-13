f1eaeb7fb818f95e3f45285169aa0bd7e13d9e44
Dinara Abakirova <abakirovada@gmail.com>
2021-09-13 16:27:18 -0400

    PlanetResources added

---
+++ NMSWiki.Data/PlanetResources.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMSWiki.Data
+{
+    public class PlanetResources
+    {
+        [Key]
+        public int PlanetResourceId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Planets))]
+        public int PlanetId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Resources))]
+        public int ResourceId { get; set; }
+    }
+}
