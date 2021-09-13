888d512b52933f46de9e1dfa7660677a011f70f9
Dan Kallman <dkallman13@gmail.com>
2021-09-13 17:02:59 -0400

    Started resource service file

---
+++ NMSWiki.Services/ResourceService.cs
@@ -0,0 +1,29 @@
+﻿using NMSWiki.Data;
+using NMSWiki.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMSWiki.Services
+{
+    public class ResourceService
+    {
+        public bool CreateResource(ResourceCreate model)
+        {
+            var entity =
+                new Resource()
+                {
+                    Name = model.Name,
+                    Description = model.Desc
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Resources.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        
+    }
+}
