4ada13ff483bcda1204262e51dc3274bd396d465
Duncan Summers <djsummers36@gmail.com>
2021-09-13 17:00:35 -0400

    craftables service, models, and data added

---
+++ NMSWiki.Models/CraftableAdd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMSWiki.Models
+{
+    public class CraftableAdd
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public List<Ingredient> Ingredients { get; set; }
+    }
+}
