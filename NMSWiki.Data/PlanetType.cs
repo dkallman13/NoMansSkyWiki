5f22c85a4513572ed1194c4482640e44e216e9fc
Dinara Abakirova <abakirovada@gmail.com>
2021-09-13 16:56:38 -0400

    List, Create models added to PlanetType, PlanetResource and Ingredient

---
+++ NMSWiki.Data/PlanetType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMSWiki.Data
+{
+    public class PlanetType
+    {
+        [Key]
+        public int PlanetId { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
