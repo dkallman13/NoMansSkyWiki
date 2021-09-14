718154d82a6598647b1a9e3be285635c8efa31a6
Dinara Abakirova <abakirovada@gmail.com>
2021-09-14 10:06:25 -0400

    service-and-controller branch

---
+++ NMSWiki.Models/PlanetResourceDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMSWiki.Models
+{
+    public class PlanetResourceDetail
+    {
+        public int PlanetResourceId { get; set; }
+
+        public int PlanetTypeId { get; set; }
+
+        public int ResourceId { get; set; }
+    }
+}
