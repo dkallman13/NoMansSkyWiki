f1e3398b4c583d279b5e50550fecbbf3554ddefa
Dan Kallman <dkallman13@gmail.com>
2021-09-20 09:55:11 -0400

    refactored one other method to work the same way as the first

---
+++ NMSWiki.Models/PlanetResourceLookup.cs
@@ -0,0 +1,15 @@
+﻿using NMSWiki.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMSWiki.Models
+{
+    public class PlanetResourceLookup
+    {
+        public Resource resource { get; set; }
+        public PlanetResource planetResource { get; set; }
+    }
+}
